83ae3436447f540ea1b19bef595906b1f755e0ae
islajd <islajdm@gmail.com>
2021-10-04 17:25:18 +0200

    deploy application with unique stage

--- bin/code_pipeline_full_guide.js
+++ bin/code_pipeline_full_guide.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const cdk = require('@aws-cdk/core')
-const{ CodePipelineFullGuideStack } = require('../lib/code_pipeline_full_guide-stack')
+const{ CodePipelineFullGuideStack } = require('../lib/pipe/code_pipeline_full_guide-stack')
 
 const app = new cdk.App()
 new CodePipelineFullGuideStack(app, 'CodePipelineFullGuideStack', {})
