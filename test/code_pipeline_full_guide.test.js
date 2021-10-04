b96df4eb8aa3b97197129cd1d7ae9caf4f2f35f4
islajd <islajdm@gmail.com>
2021-10-04 02:49:48 +0200

    setup unit test for handler

--- test/code_pipeline_full_guide.test.js
+++ test/code_pipeline_full_guide.test.js
@@ -1,6 +1,23 @@
+/* eslint-disable no-undef */
 // const{ expect, matchTemplate, MatchStyle } = require('@aws-cdk/assert')
 // const cdk = require('@aws-cdk/core')
 // const CodePipelineFullGuide = require('../lib/code_pipeline_full_guide-stack')
+const{ handler } = require('../lib/arithmetic/handler')
 
-test('Placeholder test which never fails', () => {
+describe('Test arithmetic handler', () => {
+  it('Check status code', async () => {
+    let emptyBody = {}
+    let event = { body: emptyBody }
+    
+    const result = await handler(event)
+    expect(result.statusCode).toEqual(200)
+  })
+
+  it('Check sum', async () => {
+    let body = { a: 2, b: 2 }
+    let event = { body }
+
+    const result = await handler(event)
+    expect(result.body).toEqual(4)
+  })
 })
